51cf16a4a06101fdc7079fc67375bc9db8ef0645
Ali Karimi <a.karimi2729@gmail.com>
2024-08-14 21:34:17 +0330

    first commit

---
+++ main.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  print('''
+Hello My First CMD Game !
+my name is Ali,but you may call me Alan 
+i will try to develop and expand you
+so be right with me ! :)
+''');
+}
