995a6f1960f4d464243bae6c1cc52a3aba10eb7e
Ali Karimi <a.karimi2729@gmail.com>
2024-08-15 21:40:45 +0330

    create input functions

---
+++ lib/read.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+// makes sure that input from user is valid (not null)
+int intInput() {
+  while (true) {
+    String? input = stdin.readLineSync();
+    (input != null) ? int.parse(input) : print('Wrong input !\n Try Again');
+  }
+}
+
+String stringInput() {
+  while (true) {
+    String? input = stdin.readLineSync();
+    if (input != null) {
+      return input;
+    }
+    print('Wrong input !\n Try Again');
+  }
+}
+
+String takeName(String required) {
+  stdout.write('Enter $required name : ');
+  return stringInput();
+}
