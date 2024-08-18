1299564a504918098b107a390ca035ec7e9ac9d7
Ali Karimi <a.karimi2729@gmail.com>
2024-08-18 18:58:38 +0330

    created physic

---
+++ character/appearance/physical.dart
@@ -0,0 +1,19 @@
+/*
+  each character have height and weight and skin tone
+*/
+
+class Physical {
+  // Variable
+  final int height; // in cm
+  final int weight; // in kg
+  final String skinTone; // can be : 'white' or 'black'
+
+  // Constractor
+  Physical({
+    required this.height,
+    required this.weight,
+    required this.skinTone,
+  });
+
+  // Methods
+}
