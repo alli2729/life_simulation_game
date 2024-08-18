d5a3e2aff64f3e4e940a031349cffc4d2631fd8f
Ali Karimi <a.karimi2729@gmail.com>
2024-08-18 18:58:50 +0330

    created head

---
+++ character/appearance/head.dart
@@ -0,0 +1,16 @@
+/*
+  head of character have hair and eye
+*/
+
+import 'hair.dart';
+
+class Head {
+  // Variable
+  final String eyeColor; // can be : 'black' or 'brown' or 'blue' or 'green'
+  final Hair hair;
+
+  // Constractor
+  Head({required this.eyeColor, required this.hair});
+
+  // Methods
+}
