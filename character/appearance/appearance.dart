5a49b99f8eddbd49c912563b05139938658d721d
Ali Karimi <a.karimi2729@gmail.com>
2024-08-18 18:58:16 +0330

    created appearance

---
+++ character/appearance/appearance.dart
@@ -0,0 +1,17 @@
+/*
+  character have two object in appearance
+*/
+
+import 'head.dart';
+import 'physical.dart';
+
+class Appearance {
+  // Variable
+  final Physical physical;
+  final Head head;
+
+  // Constractor
+  Appearance({required this.physical, required this.head});
+
+  // Methods
+}
